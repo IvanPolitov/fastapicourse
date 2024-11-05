dcf4c025fc2acf673316fa2204a46ad402035ce9
Politov_Work <ivan.hanter21@mail.ru>
2024-11-05 16:57:38 +0500

    Your branch is up to date with 'origin/main'.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # fastapicourse
 
-Начинаю проходить курс по FastAPI
+Начинаю проходить курс по [FastAPI](https://stepik.org/course/179694/syllabus)
---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return FileResponse('index.html')
+
+
+@app.post('/calculate')
+async def calculate(num1: int, num2: int):
+    result = num1 + num2
+    return {'result': result}
+
+
+@app.get('/calculate')
+async def calculate(num1: int, num2: int):
+    return {'result': 'use postman'}
