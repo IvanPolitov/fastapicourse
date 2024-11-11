deba5ccf1ffdb1b544eaf4df8acd3b74c53c6130
Politov_Work <ivan.hanter21@mail.ru>
2024-11-11 11:05:40 +0500

    3.3 Headers
    Поработали с заголовками HTTP

--- app/main.py
+++ app/main.py
@@ -1,37 +1,18 @@
-from fastapi import FastAPI, Cookie, Response
-from models.models import User
-from random import randint
+from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
-sample = [
-    {'username': 'admin', 'password': 'admin'},
-    {'username': 'user', 'password': 'user'},
-]
-
-dbusers = [User(**q) for q in sample]
-
-sessions = {}
 
 
-@app.post('/login')
-async def login(user: User, response: Response):
-    for person in dbusers:
-        if user.username == person.username and user.password == person.password:
-            session_token = str(randint(0, 1000))
-            sessions[session_token] = user
-            response.set_cookie(key='session_token',
-                                value=session_token, httponly=True)
-            print(sessions)
-            return {'message': 'Cookie ready'}
-    return {'message': 'Invalid credentials'}
+@app.get('/headers')
+async def get_headers(request: Request):
+    headers = request.headers
+    print(headers)
+    if "User-Agent" not in headers:
+        raise HTTPException(status_code=400, detail="Missing User-Agent")
+    if "Accept-Language" not in headers:
+        raise HTTPException(status_code=400, detail="Missing Accept-Language")
 
-
-@app.get('/user')
-async def get_user(session_token=Cookie()):
-    print(sessions)
-    print(session_token)
-
-    user = sessions.get((session_token))
-    if user:
-        return user.dict()
-    return {'message': 'Invalid credentials'}
+    return {
+        "User-Agent": request.headers["user-agent"],
+        "Accept-Language": request.headers["accept-language"]
+    }
