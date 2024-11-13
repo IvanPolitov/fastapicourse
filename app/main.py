b6f105ad4f16fa470ce64c12ef6ac3f1de549f60
Politov_Work <ivan.hanter21@mail.ru>
2024-11-13 09:16:38 +0500

    4.1 Базовая аутентификация

--- app/main.py
+++ app/main.py
@@ -1,18 +1,44 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
+from models.models import *
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+security = HTTPBasic()
+
+USER_DATA = [
+    User(**{'username': 'user1', 'password': 'password1'}),
+    User(**{'username': 'user2', 'password': 'password2'}),
+    User(**{'username': 'user3', 'password': 'password3'}),
+]
 
 
-@app.get('/headers')
-async def get_headers(request: Request):
-    headers = request.headers
-    print(headers)
-    if "User-Agent" not in headers:
-        raise HTTPException(status_code=400, detail="Missing User-Agent")
-    if "Accept-Language" not in headers:
-        raise HTTPException(status_code=400, detail="Missing Accept-Language")
+def get_user_from_db(username: str):
+    for user in USER_DATA:
+        if user.username == username:
+            return user
+    return None
 
-    return {
-        "User-Agent": request.headers["user-agent"],
-        "Accept-Language": request.headers["accept-language"]
-    }
+
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    user = get_user_from_db(credentials.username)
+    if user is None or credentials.password != user.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid credentials',
+            headers={'WWW-Autheticate': 'Basic'}
+        )
+    return user
+
+
+@app.get('/protected_resource')
+async def get_resource(user: User = Depends(authenticate_user)):
+    return {'message': 'You got my secret, welcome', 'user': user}
+
+
+@app.get("/logout")
+def logout():
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="You have successfully logged out",
+        headers={"WWW-Authenticate": "Basic"},
+    )
